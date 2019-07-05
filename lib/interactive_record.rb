bc7574b556df4f763882cd8d909dcf68f457b210
github username <github email address>
2019-07-05 14:30:09 -0400

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -57,7 +57,7 @@ class InteractiveRecord
   end
 
 def self.find_by_name(name)
-  binding.pry
+  # binding.pry
   sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
   DB[:conn].execute(sql)
 end
